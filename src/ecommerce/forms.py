2ba95ebf02087ffb86bf7799d8d308a4eeee8fe9
Leonardo Leoni Maglia <leonardo.maglia@fatec.sp.gov.br>
2018-11-27 23:34:40 -0200

    Tela de cadastro refatorada

--- src/ecommerce/forms.py
+++ src/ecommerce/forms.py
@@ -52,10 +52,30 @@ class LoginForm(forms.Form):
 
 
 class RegisterForm(forms.Form):
-    username = forms.CharField(label='Login')
-    email = forms.EmailField()
-    password = forms.CharField(label='Senha', widget=forms.PasswordInput)
-    password2 = forms.CharField(label='Confirme sua senha', widget=forms.PasswordInput)
+    username = forms.CharField(widget=forms.TextInput(
+        attrs={
+            'class': 'input',
+            'placeholder': 'Usuario'
+        }
+    ))
+    email = forms.EmailField(widget=forms.TextInput(
+        attrs={
+            'class': 'input',
+            'placeholder': 'Email'
+        }
+    ))
+    password = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'input',
+            'placeholder': 'Senha'
+        }
+    ))
+    password2 = forms.CharField(widget=forms.PasswordInput(
+        attrs={
+            'class': 'input',
+            'placeholder': 'Confirme sua senha'
+        }
+    ))
 
     def clean_username(self):
         username = self.cleaned_data.get('username')
@@ -69,10 +89,10 @@ class RegisterForm(forms.Form):
         if qs.exists():
             raise forms.ValidationError(f"O email {email} já esta em uso!")
 
-    def clean(self):
+    def clean_password(self):
         data = self.cleaned_data
         password = self.cleaned_data.get('password')
         password2 = self.cleaned_data.get('password2')
-        if password2 != password:
-            raise forms.ValidationError("As senhas devem ser iguais!")
+        if password != password2:
+            raise forms.ValidationError(f"As senhas devem ser iguais!")
         return data
