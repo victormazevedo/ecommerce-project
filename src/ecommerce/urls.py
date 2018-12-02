e930bdea1b3d992825eac2bdef7c6c79a448f17d
Leonardo Leoni Maglia <leonardo.maglia@fatec.sp.gov.br>
2018-12-02 16:30:14 -0200

    adicionado btn exportar

--- src/ecommerce/urls.py
+++ src/ecommerce/urls.py
@@ -36,7 +36,7 @@ from .views import home_page, about_page, contact_page, login_page, register_pag
 
 urlpatterns = [
     url(r'^$', home_page, name='home'),
-    url(r'^sobre/$', about_page),
+    url(r'^sobre/$', about_page, name='about'),
     url(r'^contato/$', contact_page),
     url(r'^login/$', login_page),
     url(r'^registro/$', register_page),
